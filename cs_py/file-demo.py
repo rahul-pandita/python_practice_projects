d87c0a8dd5e36ac938ae23f670c7c8ed93de4867
Rahul Pandita <rpandita1999@outlook.com>
2024-03-02 12:47:58 +0530

    Use context manager in w mode to create new file (or overwrite existing)

--- cs_py/file-demo.py
+++ cs_py/file-demo.py
@@ -1,11 +1,2 @@
-with open("text.txt", "r") as f:
-    # reading a large file
-    size_to_read = 10
-
-    f_contents = f.read(size_to_read)
-
-    while len(f_contents) > 0:
-        print(f_contents, end="--")
-        f_contents = f.read(size_to_read)
-    
-    
+with open("text2.txt","w") as wr_file:
+    wr_file.write("BLAH")
