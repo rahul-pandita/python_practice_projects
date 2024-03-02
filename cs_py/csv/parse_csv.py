f7b02ddf5e4ab1547cfaf41d762155de36f6dbf1
Rahul Pandita <rpandita1999@outlook.com>
2024-03-02 15:21:42 +0530

    Write to a new csv file; change delimiter

--- cs_py/csv/parse_csv.py
+++ cs_py/csv/parse_csv.py
@@ -4,7 +4,10 @@ with open("names.csv", "r") as csv_file:
     csv_reader = csv.reader(csv_file)
 
     # skip over first line in an iterable using generator
-    next(csv_reader)
+    # next(csv_reader)
+
+    with open("new_names.csv", "w") as new_file:
+        csv_writer = csv.writer(new_file, delimiter="\t")
  
-    for line in csv_reader:
-        print(f"Name: {line[0]} {line[1]}")
+        for line in csv_reader:
+            csv_writer.writerow(line)
