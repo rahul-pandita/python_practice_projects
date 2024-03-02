7d49686b57756f7b1db05abf687c2a14a11db60a
Rahul Pandita <rpandita1999@outlook.com>
2024-03-02 15:13:57 +0530

    Skip over first line in an iterable (csv file) using generator

--- cs_py/csv/parse_csv.py
+++ cs_py/csv/parse_csv.py
@@ -3,5 +3,8 @@ import csv
 with open("names.csv", "r") as csv_file:
     csv_reader = csv.reader(csv_file)
 
+    # skip over first line in an iterable using generator
+    next(csv_reader)
+ 
     for line in csv_reader:
-        print(line)
+        print(f"Name: {line[0]} {line[1]}")
