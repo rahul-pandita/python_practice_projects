b551829e958282bf12d222f04c6b889540437b59
Rahul Pandita <rpandita1999@outlook.com>
2024-03-07 13:17:02 +0530

    Substitute entire urls with domain name and top level domain

--- cs_py/regex/url.py
+++ cs_py/regex/url.py
@@ -8,8 +8,12 @@ https://www.nasa.gov'''
 
 pattern = re.compile(r"https?://(www\.)?(\w+)(\.\w+)")
 
-matches = pattern.finditer(urls)
+subbed_urls = pattern.sub(r"\2\3", urls)
+
+print(subbed_urls)
+
+# matches = pattern.finditer(urls)
 
 # group 0 is everything captured
-for match in matches:
-    print(match.group(2))
+# for match in matches:
+    # print(match.group(2))
