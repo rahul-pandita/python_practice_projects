0d49fbea3ad135e34a7e30c9edfa49c9e8e8a295
Rahul Pandita <rpandita1999@outlook.com>
2024-03-07 13:19:51 +0530

    findall will return only groups if groups exist in regex pattern to search for

--- cs_py/regex/regex.py
+++ cs_py/regex/regex.py
@@ -36,7 +36,7 @@ rahul-123-pandita@my-work.net
 # pattern to search for 
 pattern = re.compile(r"[a-zA-Z0-9.-]+@[a-zA-Z-]+\.(com|edu|net)")
 
-matches = pattern.finditer(emails)
+matches = pattern.findall(emails)
 
 for match in matches:
     print(match)
