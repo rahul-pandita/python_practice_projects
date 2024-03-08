8de6e9f6b69b451ee7b38030462003037f881b75
Rahul Pandita <rpandita1999@outlook.com>
2024-03-08 13:10:12 +0530

    Write bytes to a new file(.png) in wb mode

--- cs_py/requests/req_demo.py
+++ cs_py/requests/req_demo.py
@@ -3,7 +3,8 @@ import requests
 # r = requests.get("https://xkcd.com/2903/")
 r = requests.get("https://imgs.xkcd.com/comics/earth_venus_venn_diagram.png")
 
-print(r.content)
+with open("comic.png", "wb") as f:
+    f.write(r.content)
 
 # see all of the attributes and methods available for an object
 # print(dir(r))
