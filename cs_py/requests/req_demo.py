bcb29eec316223a031e0850ca674b359f8c6c4b7
Rahul Pandita <rpandita1999@outlook.com>
2024-03-08 13:03:53 +0530

    Print out html for webpage

--- cs_py/requests/req_demo.py
+++ cs_py/requests/req_demo.py
@@ -2,4 +2,10 @@ import requests
 
 r = requests.get("https://xkcd.com/2903/")
 
-print(r)
+print(r.text)
+
+# see all of the attributes and methods available for an object
+# print(dir(r))
+
+# detailed explanation of above
+# print(help(r))
