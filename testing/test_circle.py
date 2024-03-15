31044be4e565a05ebab4ba1340b9e89d97d0e259
Rahul Pandita <rpandita1999@outlook.com>
2024-03-15 10:37:52 +0530

    Use setup method to create Circle object; test its methods

--- testing/test_circle.py
+++ testing/test_circle.py
@@ -1,13 +1,18 @@
 import pytest
 import shapes
+import math
 
 class TestCircle:
 
     def setup_method(self, method):
         print(f"Setting up {method}")
+        self.circle = shapes.Circle(10)
     
     def teardown_method(self, method):
         print(f"Tearing down {method}")
 
-    def test_one(self):
-        assert True 
+    def test_area(self):
+        assert self.circle.area() == math.pi * self.circle.radius ** 2
+
+    def test_perimeter(self):
+        assert self.circle.perimeter() == 2 * math.pi * self.circle.radius
