879fd8d34052affe05b54df6a6d2522cdcb1a33f
Rahul Pandita <rpandita1999@outlook.com>
2024-03-15 10:16:43 +0530

    Expect an error with pytest.raises()

--- testing/test_my_functions.py
+++ testing/test_my_functions.py
@@ -7,3 +7,8 @@ def test_add():
 
 def test_divide():
     assert my_functions.divide(4, 2) == 2
+
+def test_divide_zero():
+    # Expecting a division error when we call the divide function
+    with pytest.raises(ZeroDivisionError):
+        my_functions.divide(1, 0)
