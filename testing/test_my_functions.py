4c34fb11f384cb0884d1998a21f49cd8c1b99d4f
Rahul Pandita <rpandita1999@outlook.com>
2024-03-15 10:10:49 +0530

    Basic pytest example; throwing a simple assert statement

--- testing/test_my_functions.py
+++ testing/test_my_functions.py
@@ -3,3 +3,7 @@ import my_functions
 
 def test_add():
     assert my_functions.add(2, 3) == 5
+    # assert my_functions.add(2, 2) == 5
+
+def test_divide():
+    assert my_functions.divide(4, 2) == 2
