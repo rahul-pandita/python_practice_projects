0a208e8358b0c5a3a1aebd01b9f7627d75b98c18
Rahul Pandita <rpandita1999@outlook.com>
2024-03-16 11:06:02 +0530

    Get url of image; write bytes to a new file; creates new image

--- practice.py
+++ practice.py
@@ -2,9 +2,18 @@ import requests
 import json
 import sys
 
-r = requests.get("https://xkcd.com/2907/")
+# r = requests.get("https://xkcd.com/2907/")
 
-print(r.text)
+# print(r.text)
+
+# PRINTING OUT IMAGE    
+response = requests.get("https://imgs.xkcd.com/comics/schwa.png")
+
+# content of response in bytes
+# print(response.content)
+
+with open("image.jpeg", "wb") as comic:
+    comic.write(response.content)
 
 # status code
 # print(r)
