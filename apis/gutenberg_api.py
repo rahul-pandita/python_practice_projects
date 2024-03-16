47319281d35c1487734d0389d71d6140d00262de
Rahul Pandita <rpandita1999@outlook.com>
2024-03-16 17:40:48 +0530

    print name of authors on first page of query

--- apis/gutenberg_api.py
+++ apis/gutenberg_api.py
@@ -6,5 +6,10 @@ response = requests.get("https://gutendex.com/books")
 
 b = response.json()
 
+results = b["results"]
+
 with open("book_list.txt", "r") as book:
-    print(b["results"][0]["authors"][0]["name"])
+    for result in results:
+        print(result["authors"][0]["name"])
+
+    # print(b["results"][0]["authors"][0]["name"])
