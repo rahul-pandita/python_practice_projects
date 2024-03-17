8e844b2a2effda469519059f6b79ef94472a8bb4
Rahul Pandita <rpandita1999@outlook.com>
2024-03-17 10:49:23 +0530

    Create simple function to get json response from gutenberg api

--- apis/gutenberg_api.py
+++ apis/gutenberg_api.py
@@ -2,14 +2,9 @@ import requests
 import json
 import sys
 
-response = requests.get("https://gutendex.com/books")
+def get_api_response():
+    # GETS JSON FROM GUTENBERG API AS A DICT
+    response = requests.get("https://gutendex.com/books")
+    print(json.dumps(response.json(), indent=2))
 
-b = response.json()
-
-results = b["results"]
-
-with open("book_list.txt", "r") as book:
-    for result in results:
-        print(result["authors"][0]["name"])
-
-    # print(b["results"][0]["authors"][0]["name"])
+get_api_response()
