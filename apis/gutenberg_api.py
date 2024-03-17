55f868e44785c7785bf0cc9579554b00f304f633
Rahul Pandita <rpandita1999@outlook.com>
2024-03-17 11:17:56 +0530

    Create function to display all available book titles

--- apis/gutenberg_api.py
+++ apis/gutenberg_api.py
@@ -5,14 +5,23 @@ import sys
 def get_api_response():
     # GETS JSON FROM GUTENBERG API AS A DICT
     response = requests.get("https://gutendex.com/books").json()
-    return response
-    # print(json.dumps(response.json(), indent=2))
-
-# get_api_response()
     
-def display_book_details(response):
     # Display all the details of books available on page queried
     book_details = response["results"]
-    print(book_details)
 
-display_book_details(get_api_response())
+    # return a list containing all the detail for available books
+    return book_details
+
+
+def display_book_titles(books_list):
+    book_titles = []
+
+    # print out all available books
+    for book in books_list:
+        book_titles.append(book["title"])
+        
+    print("\n".join(book_titles))
+    return book_titles
+
+
+display_book_titles(get_api_response())
