f2820d4f4b9af69d5036207cf821cb6e5076e014
Rahul Pandita <rpandita1999@outlook.com>
2024-03-17 10:57:25 +0530

    Create a function to display all available details of all the books available on page queried as a list

--- apis/gutenberg_api.py
+++ apis/gutenberg_api.py
@@ -4,7 +4,15 @@ import sys
 
 def get_api_response():
     # GETS JSON FROM GUTENBERG API AS A DICT
-    response = requests.get("https://gutendex.com/books")
-    print(json.dumps(response.json(), indent=2))
+    response = requests.get("https://gutendex.com/books").json()
+    return response
+    # print(json.dumps(response.json(), indent=2))
 
-get_api_response()
+# get_api_response()
+    
+def display_book_details(response):
+    # Display all the details of books available on page queried
+    book_details = response["results"]
+    print(book_details)
+
+display_book_details(get_api_response())
