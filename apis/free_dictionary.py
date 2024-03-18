0ab3d30ee0c8b02012cc65547cefa9233f31aa99
Rahul Pandita <rpandita1999@outlook.com>
2024-03-18 21:36:36 +0530

    Create function to extract defintions from the json response

--- apis/free_dictionary.py
+++ apis/free_dictionary.py
@@ -3,19 +3,24 @@ import sys
 import json
 
 def main():
-    print(get_user_word())
+    get_definitions(get_user_word())
     ...
 
 def get_user_word():
     while True:
-        try:
-            word = input("Which word's meaning would you like to know? ")
-            if not word.isalpha():
-                print("Please enter a valid word")
-                continue
-            return word
-        except:
-            ...
+        word = input("Which word's meaning would you like to know? ")
+        if not word.isalpha():
+            print("Please enter a valid word")
+            continue
+        return word
+
+def get_definitions(word):
+    response = requests.get("https://api.dictionaryapi.dev/api/v2/entries/en/" + word).json()
+    
+    print("Our database has the following definitions for your word:")
+    for count, defintion in enumerate(response[0]["meanings"][0]["definitions"]):
+        print(f"Definition {count + 1}:", defintion["definition"])
+
 
 if __name__ == "__main__":
     main()
